c6dd4214b659a6a4537264ce4e1eaed2ca79991f
Robert120697 <90307385+Robert120697@users.noreply.github.com>
2021-09-13 14:09:33 +0700

    Add files via upload

---
+++ Assets/BallControler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControler : MonoBehaviour
+{
+    Text ScoreUIP1, ScoreUIP2;
+    int ScoreP1,ScoreP2;
+
+    public int force;
+    Rigidbody2D rigid;
+    void Start()
+    {
+        ScoreP1 = 0;
+        ScoreP2 = 0;
+        ScoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
+        ScoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2, 0).normalized;
+        rigid.AddForce(arah * force);
+    }
+    void TampilkanScore()
+    { ScoreUIP1.text = ScoreP1 + "";
+      ScoreUIP2.text = ScoreP2 + "";
+    }
+    void ResetBall()
+    { transform.localPosition = new Vector2(0, 0); 
+      rigid.velocity =new Vector2(0, 0);}
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "tepiKiri")
+        {
+            ScoreP2 += 1;
+            TampilkanScore();
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);}
+        if (coll.gameObject.name == "tepiKanan")
+        {
+            ScoreP1 += 1;
+            TampilkanScore();
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);}
+        if (coll.gameObject.name == "Player1" || coll.gameObject.name == "Player2")
+        { float sudut = (transform.position.y - coll.transform.position.y) * 5f;
+            Vector2 arah = new Vector2(rigid.velocity.x, sudut).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(arah * force * 2);
+        }
+    }
+
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float batasAtas;
+    public float batasBawah;
+    public float speed;
+    public string axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak = Input.GetAxis(axis) * speed * Time.deltaTime;
+        float nextPos = transform.position.y + gerak;
+        if (nextPos > batasAtas)
+        { gerak = 0; }
+        if (nextPos < batasBawah)
+        { gerak = 0; }
+        transform.Translate(0, gerak, 0);
+    }
+}
